61cc3261d656a5eef44debfbff2608b977bae9d5
Jack Chen <chejac@seas.upenn.edu>
2023-04-25 22:29:29 -0400

    Hook Implementation

--- client/src/pages/ParametersSearchPage.js
+++ client/src/pages/ParametersSearchPage.js
@@ -26,10 +26,10 @@ export default function ParametersSearchPage() {
       <Box mt={3} mb={3} p={3} sx={{ background: 'black', borderRadius: '16px'}} >
         <h2>Find all housing, demographics, and economic info for a particular zip code</h2>
         <p>Enter the following parameters and search:</p>
-        <TextField id="outlined-basic" label="Zipcode" variant="outlined" required inputProps={{maxLength: 5}} onChange={(e) => setselectedZipcode(e.target.value)}/>
+        <TextField id="outlined-basic" label="Zipcode" variant="outlined" required inputProps={{maxLength: 5}} onChange={(e) => setZipcode(e.target.value)}/>
 
         <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
-          <Button variant="outlined" onClick={() => searchRoute1() } sx={{ height: 40 }}> Search </Button>
+          <Button variant="outlined" onClick={() => search() } sx={{ height: 40 }}> Search </Button>
         </Box>
       </Box>
 
